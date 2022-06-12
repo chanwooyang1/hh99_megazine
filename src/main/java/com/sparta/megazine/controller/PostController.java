8f71d718662b3908ca933b3e866c8850abc867e1
chanwoo yang <moomsss@naver.com>
2022-06-12 20:43:11 +0900

    post crud 생성

--- src/main/java/com/sparta/megazine/controller/PostController.java
+++ src/main/java/com/sparta/megazine/controller/PostController.java
@@ -1,8 +1,11 @@
 package com.sparta.megazine.controller;
 
+import com.sparta.megazine.domain.Post;
 import com.sparta.megazine.repository.PostRepository;
 import com.sparta.megazine.service.PostService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -15,4 +18,11 @@ public class PostController {
 
 
 //    CRUD 추가해야함
+
+    @GetMapping("/api/posts/{postId}")
+    public Post getPost(@PathVariable Long postID){
+        return postRepository.findById(postID).orElseThrow(() -> new IllegalArgumentException("게시글 아이디가 존재하지 않습니다."));
+
+    }
+
 }
