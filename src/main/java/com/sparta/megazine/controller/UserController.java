77a8f5ea8e588ffea35a093b33ad04fdf487c882
chanwoo yang <moomsss@naver.com>
2022-06-17 00:03:30 +0900

    User기능 완료 회원가입, 로그인, 회원정보보기

--- src/main/java/com/sparta/megazine/controller/UserController.java
+++ src/main/java/com/sparta/megazine/controller/UserController.java
@@ -1,53 +1,79 @@
 package com.sparta.megazine.controller;
 
 
+
 import com.sparta.megazine.domain.User;
-import lombok.RequiredArgsConstructor;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.web.bind.annotation.*;
+import com.sparta.megazine.dto.UserRequestDto;
+
 import com.sparta.megazine.repository.UserRepository;
-import com.sparta.megazine.security.JwtTokenProvider;
-import java.util.Collections;
-import java.util.Map;
+
+import com.sparta.megazine.service.UserService;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+
+
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDateTime;
+
 
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/api")
 public class UserController {
 
-    private final PasswordEncoder passwordEncoder;
-    private final JwtTokenProvider jwtTokenProvider;
+
+    private final UserService userService;
     private final UserRepository userRepository;
+
 
     // 회원가입
     @PostMapping("/register")
-    public Long join(@RequestBody Map<String, String> user) {
-        return userRepository.save(User.builder()
-                .profile_image_url(user.get("profile_image_url"))
-                .introduce(user.get("introduce"))
-                .nickname(user.get("nickname"))
-                .name(user.get("name"))
-                .email(user.get("email"))
-                .password(passwordEncoder.encode(user.get("password")))
-                .roles(Collections.singletonList("ROLE_USER")) // 최초 가입시 USER 로 설정
-                .build()).getId();
+    public boolean register(@RequestBody UserRequestDto.Register userRegister) {
+        return userService.register(userRegister);
+
     }
 
     // 로그인
     @PostMapping("/login")
-    public String login(@RequestBody Map<String, String> user) {
-        User user1 = userRepository.findByEmail(user.get("email"))
-                .orElseThrow(() -> new IllegalArgumentException("가입되지 않은 E-MAIL 입니다."));
-        if (!passwordEncoder.matches(user.get("password"), user1.getPassword())) {
-            throw new IllegalArgumentException("잘못된 비밀번호입니다.");
+    public String login(@RequestBody UserRequestDto.Login userLogin) {
+
+        return userService.login(userLogin);
+    }
+
+    //해결 못해서 팀원분꺼 복붙
+    @GetMapping("/user")
+    public UserInfo userInfo(@AuthenticationPrincipal User user) {
+
+        User findUser = userRepository.findById(user.getId()).orElseThrow(()
+                -> new IllegalArgumentException("유저 정보가 존재하지 않음"));
+        return new UserInfo(findUser);
+    }
+
+    //ResponseUser 필요
+    @Data
+    @AllArgsConstructor
+    static class UserInfo {
+        private String name;
+        private String nickname;
+        private String email;
+        private String introduce;
+        private String profile_image_url;
+        private LocalDateTime createdAt;
+
+
+        public UserInfo(User user) {
+            this.name = user.getName();
+            this.nickname = user.getNickname();
+            this.email = user.getEmail();
+            this.profile_image_url = user.getProfile_image_url();
+            this.introduce = user.getIntroduce();
+            this.createdAt = user.getCreatedAt();
+
         }
-        return jwtTokenProvider.createToken(user1.getUsername(), user1.getRoles());
     }
 }
-
-//    @GetMapping("/user")
-//    public User getUserInfo(@RequestBody UserRepository userRepository){
-//
-//    }
-//}
 
